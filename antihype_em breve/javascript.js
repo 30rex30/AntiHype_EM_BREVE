bc3d20360ab4e5a0b30ac6bbd2ca725b17e05e60
Renato Bento <167688826+30rex30@users.noreply.github.com>
2025-12-06 22:42:13 +0000

    Add files via upload

---
+++ antihype_em breve/javascript.js
@@ -0,0 +1,8 @@
+
+const EMAILJS_CONFIG = {
+    SERVICE_ID: process.env.EMAILJS_SERVICE_ID || 'Oficial.AntiHype',
+    TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID || 'template_cxt3oo6',
+    PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY || '1rLmwo9G-nA7GuKqe'
+};
+
+module.exports = EMAILJS_CONFIG;
